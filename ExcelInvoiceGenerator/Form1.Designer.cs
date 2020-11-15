53f624ffed4f0284ce22816c647b877f37aecfbc
Abdul Moiz <moiza0799@gmail.com>
2020-11-15 16:38:55 +0500

    Incomplete

--- ExcelInvoiceGenerator/Form1.Designer.cs
+++ ExcelInvoiceGenerator/Form1.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmb_PartyName = new SergeUtils.EasyCompletionComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lab_Address = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_UploadSKU = new System.Windows.Forms.Button();
+            this.btn_GenerateInvoice = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmb_PartyName
+            // 
+            this.cmb_PartyName.FormattingEnabled = true;
+            this.cmb_PartyName.Location = new System.Drawing.Point(141, 71);
+            this.cmb_PartyName.Name = "cmb_PartyName";
+            this.cmb_PartyName.Size = new System.Drawing.Size(340, 29);
+            this.cmb_PartyName.TabIndex = 1;
+            this.cmb_PartyName.SelectedIndexChanged += new System.EventHandler(this.cmb_PartyName_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Party Name:";
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 25.81132F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1093, 45);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NEW INVOICE";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(487, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Create New Party";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 14.26415F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 464);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1093, 43);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "  Current Invoice No: 0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lab_Address);
+            this.groupBox1.Location = new System.Drawing.Point(491, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(562, 294);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Party Details";
+            // 
+            // lab_Address
+            // 
+            this.lab_Address.AutoSize = true;
+            this.lab_Address.Location = new System.Drawing.Point(21, 40);
+            this.lab_Address.Name = "lab_Address";
+            this.lab_Address.Size = new System.Drawing.Size(106, 21);
+            this.lab_Address.TabIndex = 3;
+            this.lab_Address.Text = "Party Name:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(141, 113);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(344, 27);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 21);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Date:";
+            // 
+            // btn_UploadSKU
+            // 
+            this.btn_UploadSKU.Location = new System.Drawing.Point(141, 151);
+            this.btn_UploadSKU.Name = "btn_UploadSKU";
+            this.btn_UploadSKU.Size = new System.Drawing.Size(165, 33);
+            this.btn_UploadSKU.TabIndex = 9;
+            this.btn_UploadSKU.Text = "Upload";
+            this.btn_UploadSKU.UseVisualStyleBackColor = true;
+            // 
+            // btn_GenerateInvoice
+            // 
+            this.btn_GenerateInvoice.Location = new System.Drawing.Point(321, 151);
+            this.btn_GenerateInvoice.Name = "btn_GenerateInvoice";
+            this.btn_GenerateInvoice.Size = new System.Drawing.Size(165, 33);
+            this.btn_GenerateInvoice.TabIndex = 10;
+            this.btn_GenerateInvoice.Text = "Invoice";
+            this.btn_GenerateInvoice.UseVisualStyleBackColor = true;
+            this.btn_GenerateInvoice.Click += new System.EventHandler(this.btn_GenerateInvoice_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1093, 507);
+            this.Controls.Add(this.btn_GenerateInvoice);
+            this.Controls.Add(this.btn_UploadSKU);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmb_PartyName);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.86792F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InvoiceGenerator";
+            this.Load += new System.EventHandler(this.cmb_PartyName_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private SergeUtils.EasyCompletionComboBox cmb_PartyName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lab_Address;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_UploadSKU;
+        private System.Windows.Forms.Button btn_GenerateInvoice;
     }
 }
 
--- ExcelInvoiceGenerator/Form1.cs
+++ ExcelInvoiceGenerator/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,159 @@ namespace ExcelInvoiceGenerator
 {
     public partial class Form1 : Form
     {
+        double quantity = 0, basePrice = 0, SGST = 0, CGST = 0, IGST = 0, rate = 0, amount = 0, TotalAmount = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void cmb_PartyName_Load(object sender, EventArgs e)
+        {
+
+            string[] lineOfContents = File.ReadAllLines(Application.StartupPath + "\\PartyDetails.csv");
+            foreach (var line in lineOfContents)
+            {
+                string[] tokens = line.Split(',');
+                cmb_PartyName.Items.Add(tokens[1]);
+            }
+            btn_GenerateInvoice_Click(sender, e);
+        }
+
+        private void cmb_PartyName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = cmb_PartyName.SelectedIndex + 1;
+            string[] lineOfContents = File.ReadAllLines(Application.StartupPath + "\\PartyDetails.csv");
+            foreach (var line in lineOfContents)
+            {
+                string[] tokens = line.Split(',');
+                if (tokens[0] == index.ToString())
+                {
+                    lab_Address.Text = tokens[2];
+                }
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Process.Start(Application.StartupPath + "\\PartyDetails.csv");
+        }
+
+        private void btn_GenerateInvoice_Click(object sender, EventArgs e)
+        {
+
+
+            XLWorkbook Workbook = new XLWorkbook(Application.StartupPath + "\\template.xlsx");
+            IXLWorksheet Worksheet = Workbook.Worksheet(1);
+            Worksheet.Cell("A1").Value = "TAX INVOICE";
+            Worksheet.Cell("A2").Value = "Billed From:-";
+            Worksheet.Cell("A3").Value = "HUBBERHOLME";
+            Worksheet.Cell("A4").Value = "A-13, Lower Ground Foor, Sector 58, Noida 201301";
+            Worksheet.Cell("A5").Value = "GSTIN:  09CMFPS6001N2ZM";
+            Worksheet.Cell("A6").Value = "PH: 0091-9582796098";
+            Worksheet.Cell("A7").Value = "BILL TO:";
+
+            Worksheet.Cell("F2").Value = "INVOICE NO.";
+            Worksheet.Cell("F4").Value = "BUYER'S ORDER NO. & DATE:	";
+            Worksheet.Cell("F7").Value = "Ship To:-";
+
+            Worksheet.Cell("H4").Value = "PARTY NAME AS PER BOOKS";
+
+            Worksheet.Cell("I2").Value = "Date";
+            Worksheet.Cell("I3").Value = dateTimePicker1.Value.ToShortDateString();
+
+            //Invoice table header
+
+            Worksheet.Cell("A15").Value = "SKU		";
+            Worksheet.Cell("D15").Value = "QTY";
+            Worksheet.Cell("E15").Value = "BASE PRICE";
+            Worksheet.Cell("F15").Value = "TOTAL TAXABLE VALUE";
+            Worksheet.Cell("G15").Value = "SGST";
+            Worksheet.Cell("H15").Value = "CGST";
+            Worksheet.Cell("I15").Value = "IGST";
+            Worksheet.Cell("J15").Value = "RATE";
+            Worksheet.Cell("K15").Value = "AMOUNT";
+
+            int index = cmb_PartyName.SelectedIndex + 1;
+            string[] lineOfContents = File.ReadAllLines(@"C:\Users\moiza\Desktop\SKULIST.csv");
+            int startIndexForSku = 16;
+            foreach (var line in lineOfContents)
+            {
+                string[] tokens = line.Split(',');
+                Worksheet.Cell("A" + startIndexForSku).Value = tokens[0]; //suk
+                Worksheet.Cell("D" + startIndexForSku).Value = tokens[1];//quantity
+                double.TryParse(tokens[1], out quantity);
+
+                // to merge cell
+                var range = Worksheet.Range("A" + startIndexForSku + ":C" + startIndexForSku);
+                range.Merge();
+                //to set border
+                var borderRange = Worksheet.Range("A" + startIndexForSku + ":K" + startIndexForSku);
+                borderRange.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+                borderRange.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                //find sku in the database
+                string[] sku = File.ReadAllLines(@"C:\Users\moiza\Desktop\SKUDetails.csv");
+
+                foreach (var item in sku)
+                {
+                    string[] skus = item.Split(',');
+                    if (skus[0] == tokens[0])
+                    {
+                        double.TryParse(skus[1], out basePrice);
+                        double.TryParse(skus[2], out SGST);
+                        double.TryParse(skus[3], out CGST);
+                        double.TryParse(skus[4], out IGST);
+                        double.TryParse(skus[5], out rate);
+
+                        amount = basePrice + SGST + CGST + IGST;
+                        TotalAmount += amount * quantity;
+                        Worksheet.Cell("E" + startIndexForSku).Value = basePrice; // base price
+                        Worksheet.Cell("F" + startIndexForSku).Value = basePrice; // total taxable value
+                        Worksheet.Cell("G" + startIndexForSku).Value = SGST; //SGST
+                        Worksheet.Cell("H" + startIndexForSku).Value = CGST; //CGST
+                        Worksheet.Cell("I" + startIndexForSku).Value = IGST; //IGST
+                        Worksheet.Cell("J" + startIndexForSku).Value = rate + "%"; //Rate
+                        Worksheet.Cell("J" + startIndexForSku).SetDataType(XLDataType.Number);
+                        Worksheet.Cell("J" + startIndexForSku).Style.NumberFormat.Format = "0.00%";
+                        Worksheet.Cell("K" + startIndexForSku).Value = amount; //amount
+
+                    }
+                }
+                startIndexForSku++;
+                if (tokens[0] == index.ToString())
+                {
+                    lab_Address.Text = tokens[2];
+                }
+            }
+            startIndexForSku++;
+
+            // total amount in words
+            // to merge cell
+            var range1 = Worksheet.Range("D" + startIndexForSku + ":H" + startIndexForSku);
+            range1.Merge();
+            //to set border
+            var borderRange2 = Worksheet.Range("B" + startIndexForSku + ":K" + startIndexForSku);
+            borderRange2.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+            borderRange2.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+            
+            string amountInWords = NumberToWords.ConvertAmount(Math.Round(TotalAmount, 0));
+            Worksheet.Cell("B" + startIndexForSku).Value = "In Words";
+            Worksheet.Cell("D" + startIndexForSku).Value = amountInWords;
+            Worksheet.Cell("I" + startIndexForSku).Value = "TOTAL";
+            Worksheet.Cell("K" + startIndexForSku).Value = TotalAmount;
+
+
+            //signature and date
+            startIndexForSku++;
+            startIndexForSku++;
+            Worksheet.Cell("H" + startIndexForSku).Value = "Signature & Date:";
+            startIndexForSku++;
+            startIndexForSku++;
+            startIndexForSku++;
+            Worksheet.Cell("H" + startIndexForSku).Value = "\tFOR HUBBERHOLME";
+
+
+            Workbook.SaveAs(@"C:\Users\moiza\Desktop\file.xlsx");
+
         }
     }
 }
